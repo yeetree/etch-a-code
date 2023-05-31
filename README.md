4800cbd99edc6be9ea6ad71855df541019a27c02
yeetree <64941543+yeetree@users.noreply.github.com>
2023-05-30 22:49:39 -0500

    v2.1 - Add Thue, Remove JS, Etc
    Added Thue, Removed JavaScript, Added some other functions for the terminal. Established IDE as the ultimate IDE for
    Esoteric programming languages... in the making...

--- README.md
+++ README.md
@@ -9,4 +9,18 @@ If you want to, you can save your code by exporting it. However, you cannot impo
 
 Like other IDEs, you can run code in it too!
 Click on the "Run" button at the top, select your language, and it's good to go!
-Currently, we have full support for BrainF**k, and semi-support for JavaScript.
+Currently, it has support for BrainF**k and Thue, and I'll be adding more soon!
+
+Unlike other online esoteric programming language runners, this IDE actually has keyboard input! No more typing your input into a lazy box below the console, you can type it straight in!
+
+This used to have JavaScript, but it was tricky getting it to work in the custom console,
+and it was causing lots of problems, so I ditched it to make this the ultimate IDE for esoteric programming languages.
+---
+### TODO / Current Problems
+For some reason, you have to click "Run" twice to get your BrainF**k program to run properly,
+I haven't tested this for Thue.
+
+I need to make the step-time adjustable. If there is no step time, it will run fast, but will
+get caught in infinite loops and crash, but if too high, simple programs will take ages to run.
+
+And I want to add more programming languages, but it's difficult because I'm implementing them in pure, top-level JavaScript.
--- terminal.js
+++ terminal.js
@@ -1,3 +1,25 @@
+extime = 5;
+cancel = false;
+
+ascii = [
+    '&#00;','&#01;','&#02;','&#03;','&#04;','&#05;','&#06;','&#;07',
+    '&#08;','&#09;','&#10;','&#11;','&#12;','&#13;','&#14;','&#15;',
+    '&#16;','&#17;','&#18;','&#19;','&#20;','&#21;','&#22;','&#23;',
+    '&#24;','&#25;','&#26;','&#27;','&#28;','&#29;','&#30;','&#31',
+    ' ', '!', '"', '#', '$', '%', '&', '\'',
+    '(', ')', '*', '+', ',', '-', '.', '/',
+    '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', ':', ';', '<', '=', '>', '?',
+    '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'L', 'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z', '[', '\\' ,']', '^', '_',
+    '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z', '{', '|', '}', '~', ''
+];
+
 function waitkey()
 {
     var term = document.getElementById("termtxt")
@@ -14,28 +36,97 @@ function waitkey()
     })
 }
 
+function waitkeybf()
+{
+    var term = document.getElementById("termtxt")
+    return new Promise((resolve) => {
+        term.addEventListener("keydown", keyhandler);
+        function keyhandler(e)
+        {
+            if(e.key.length == 1)
+            {
+                term.removeEventListener("keydown", keyhandler)
+                resolve(e.key);
+            }
+            else if(e.key == "Enter")
+            {
+                term.removeEventListener("keydown", keyhandler)
+                resolve(ascii[13]);
+            }
+        }
+    })
+}
+
+function waitstr()
+{
+    var term = document.getElementById("termtxt")
+    return new Promise((resolve) => {
+        var str = ""
+        term.addEventListener("keydown", keyhandler);
+        function keyhandler(e)
+        {
+            if(e.key.length == 1)
+            {
+                str += e.key;
+            }
+            else if(e.key == "Enter")
+            {
+                term.removeEventListener("keydown", keyhandler)
+                resolve(str);
+            }
+        }
+    })
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function run()
 {
     select = document.getElementById("prg").value;
-    if(select == "js")
+    cancel = true;
+    switch(select)
     {
-        await js(file);
+        case "js":
+            cancel = false;
+            await js(file);
+            break;
+        case "bf":
+            cancel = false;
+            await brainfk(file);
+            break;
+        case "thue":
+            cancel = false;
+            prg = file.join("\n");
+            thuep = await thueparse(prg);
+            await thue(thuep.rules, thuep.state);
     }
-    else if(select == "bf")
-    {
-        await brainfk(file)
-    }
-
 }
 
+/*
 async function js(txt)
 {
     var term = document.getElementById("termtxt")
+    console.oldLog = console.log;
+    console.olderr = console.error;
+
+    console.error = function(err)
+    {
+        term.innerHTML += "ERROR: " + value + "\n"
+    }
+
+    console.log = function(value)
+    {
+        //console.oldLog(value);
+        term.innerHTML += value + "\n"
+    };
+    
+    const output = eval('console.log("hello")');
     term.innerHTML = "";
     prg = txt.join("\n");
     out = eval(prg);
-    term.innerHTML ="Check the Debug Console, this feature is not implemented for JavaScript yet."
-}
+}*/
 
 async function brainfk(txt)
 {   
@@ -44,32 +135,13 @@ async function brainfk(txt)
     
     prg = txt.join("");
 
-    ptr = 0
+    ptr = 0;
     max = 65536
     mem = Array(max).fill(0);
     prgptr = 0;
     braces = []
 
-    ascii = [
-        '<NUL>','<SOH>','<STX>','<ETX>','<EOT>','<ENQ>','<ACK>','<BEL>',
-        '<BS>' ,'<HT>' ,'<LF>' ,'<VT>' ,'<NP>' ,'<CR>' ,'<SO>' ,'<SI>' ,
-        '<DLE>','<DC1>','<DC2>','<DC3>','<DC4>','<NAK>','<SYN>','<ETB>',
-        '<CAN>','<EM>' ,'<SUB>','<ESC>','<FS>' ,'<GS>' ,'<RS>' ,'<US>' ,
-        ' ', '!', '"', '#', '$', '%', '&', '\'',
-        '(', ')', '*', '+', ',', '-', '.', '/',
-        '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', ':', ';', '<', '=', '>', '?',
-        '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-        'P', 'Q', 'L', 'S', 'T', 'U', 'V', 'W',
-        'X', 'Y', 'Z', '[', '\\' ,']', '^', '_',
-        '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-        'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-        'x', 'y', 'z', '{', '|', '}', '~', 'DEL'
-    ];
-
-    while(prgptr < prg.length)
+    while(prgptr < prg.length && !cancel)
     {   
         c = prg[prgptr];
 
@@ -100,7 +172,7 @@ async function brainfk(txt)
                 prgptr++
                 break;
             case ",":
-                char = await waitkey();
+                char = await waitkeybf();
                 mem[ptr] = ascii.indexOf(char);
                 prgptr++
                 break;
@@ -129,6 +201,84 @@ async function brainfk(txt)
                 break;
                     
         }
-        console.log(mem[ptr] + " " + prg[prgptr]);
+        await sleep(extime);
+        console.log(mem[ptr] + " " + prg[prgptr] + " " + prgptr);
     }
+}
+
+async function thueparse(code) {
+    code = code.split(/\n::=\n/); //Maybe match /\n::=\n
+    let rules = code.shift();
+    rules = parseRules(rules);
+    const state = code.join("\n::=\n");
+    return {
+        rules: rules,
+        state: state
+    };
+};
+
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+async function thue(rules, state) {
+    var term = document.getElementById("termtxt")
+    term.innerHTML = "";
+    let finished = false;
+
+    while (!finished && !cancel) {
+        const possibleRules = rules.filter((rule) => {
+            return state.includes(rule[0]);
+        });
+        if (possibleRules.length === 0) {
+            break;
+        }
+        const randomRule = getRandom(possibleRules);
+
+        const matches = [];
+        let index = 0;
+        while (index >= 0) {
+            index = state.indexOf(randomRule[0], index);
+            if (index >= 0) {
+                matches.push(index);
+                index++;
+            }
+        }
+
+        const randomMatch = getRandom(matches);
+        const stateA = state.slice(0, randomMatch);
+        const stateB = state.slice(randomMatch + randomRule[0].length);
+        let value = "";
+        if (randomRule[1][0] === "~") {
+            term.innerHTML += randomRule[1].substr(1);
+        } else if (randomRule[1] === ":::") {
+            value = await waitstr();
+        } else {
+            value = randomRule[1];
+        }
+        state = stateA + value + stateB;
+        await sleep(extime);
+    }
+    return state;
+};
+
+function parseRules(rules) {
+    rules.trim();
+    rules = rules.split("\n");
+    rules = rules.map((rule) => {
+        if (!rule || rule === null) return null;
+        rule = rule.split("::=");
+        if (rule.length < 2) {
+            console.error("Invalid rule", rule);
+            return null;
+        }
+        const result = [];
+        result.push(rule.shift());
+        result.push(rule.join("::="));
+        return result;
+    }).filter((rule) => {
+        if (!rule) return false;
+        else return true;
+    });
+    return rules;
 }
